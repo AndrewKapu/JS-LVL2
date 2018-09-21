8e04e9cbda16062cba25c870bca05d365097ad9d
Andrew <andrewinapple@gmail.com>
2018-09-21 18:12:22 +0300

    Создан метод getInputValue

--- task3/validation.js
+++ task3/validation.js
@@ -1,15 +1,20 @@
 class ValidationForm {
     constructor(inputIDs, validationRegExes) {
-        this.inputIDs = inputIDs;
+        this.inputIDs = inputIDs;// type string
         this.validationRegExes = validationRegExes;
     }
 
     init() {
         for (let i = 0; i < this.inputIDs.length; i++) {
 
-            if (this.validationRegExes[i].test(getInput(this.inputIDs[i]))) {
+            if (this.validationRegExes[i].test(this.getInputValue(this.inputIDs[i]))) {
 
             }
         }
+    }
+
+    getInputValue(ID) {
+        let input = document.getElementById(ID);
+        return input.value;
     }
 }
