4c0b825cfc7fb40494bd3a736e719557a4d9dfca
Andrew <andrewinapple@gmail.com>
2018-09-23 18:29:37 +0300

    Реализация валидации формы завершена

--- task3/validation.js
+++ task3/validation.js
@@ -1,20 +1,90 @@
+/**
+ * Класс объекта валидации формы
+ */
 class ValidationForm {
-    constructor(inputIDs, validationRegExes) {
-        this.inputIDs = inputIDs;// type string
-        this.validationRegExes = validationRegExes;
+    /**
+     * @constructor Функция конструктор валидации формы
+     * @param {string} form ID формы
+     * @property {object} patterns Содержит необходимые нам шаблоны для валидации формы
+     * @property {object} errors Содержит сообщения об ошибках
+     * @property {string} errorClass класс для обозначения ошибки в поле
+     */
+    constructor(form) {
+        this.patterns = {
+            name: /^\w+$/,
+            phone: /^\+7\(\d{3}\)\d{3}-\d{4}$/,
+            email: /^[\w.-_]+@\w+\.\w{2,4}$/
+        };
+        this.errors = {
+            name: 'Имя содержит только буквы',
+            phone: 'Телефон подчиняется шаблону +7(000)000-0000',
+            email: 'E-mail выглядит как mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru'
+        };
+        this.errorClass = 'error-msg';
+        this.form = form;
+        this._validateForm(this.form);
     }
 
-    init() {
-        for (let i = 0; i < this.inputIDs.length; i++) {
+    /**
+     * Функкция валидирует форму
+     * @param {string} form ID нашей формы
+     * @private
+     */
+    _validateForm(form){
+        let formFields = [...document.getElementById(form).getElementsByTagName('input')];
+        for (let field of formFields){
+            this._validate(field);
+        }
+    }
 
-            if (this.validationRegExes[i].test(this.getInputValue(this.inputIDs[i]))) {
-
+    /**
+     * Функция валидирует поля формы
+     * @param {HTMLElement} field input, который мы валидируем
+     * @private
+     */
+    _validate(field){
+        if (this.patterns[field.name]){
+            if (!this.patterns[field.name].test(field.value)) {
+                field.classList.toggle('error-alert');
+                this._addErrorMsg(field);
+                this._watchField(field);
             }
         }
     }
 
-    getInputValue(ID) {
-        let input = document.getElementById(ID);
-        return input.value;
+    /**
+     * Функция добавляет сообщение об ошибке для пользователя
+     * @param {HTMLElement} field Проверяемый input
+     * @private
+     */
+    _addErrorMsg(field){
+        let errMsg = document.createElement('div');
+        errMsg.classList.add(this.errorClass);
+        errMsg.textContent = this.errors[field.name];
+        field.parentNode.appendChild(errMsg);
     }
+    /**
+     * Функция правильность при вводе
+     * @param {HTMLElement} field Проверяемый input
+     * @private
+     */
+    _watchField(field){
+        field.addEventListener('input', () => {
+            if (this.patterns[field.name].test(field.value)){
+                field.classList.remove('invalid');
+                field.classList.add('valid');
+                if (field.parentNode.lastChild !== field) {
+                    field.parentNode.lastChild.remove();
+                }
+            } else {
+                field.classList.add('invalid');
+                field.classList.remove('valid');
+                if (field.parentNode.lastChild.nodeName !== 'DIV') {
+                    this._addErrorMsg(field);
+                }
+
+            }
+        })
+    }
+
 }
